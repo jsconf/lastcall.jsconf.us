d3767982927135e686183af889e30b95ea8f017c
Chris Williams <chris@iterativedesigns.com>
2015-09-08 22:26:25 -0400

    Update.

--- js/conf.js
+++ js/conf.js
@@ -1,29 +1,37 @@
 function scrollTo(_target) {
-    var target = $(_target);
-    if (target.length)
-    {
-        var top = target.offset().top;
-        $('html,body').animate({scrollTop: top}, 1000);
-        setTimeout(function () { window.location.hash=(_target); }, 1000);
-        return false;
-    }
+  var target = $(_target);
+  if (target.length) {
+    var top = target.offset().top;
+    $('html,body').animate({
+      scrollTop: top
+    }, 1000);
+    setTimeout(function() {
+      window.location.hash = (_target);
+    }, 1000);
+    return false;
+  }
 }
 
 function activateMenu(key) {
-	$(".nav .active").removeClass("active");
-	$("#"+key).addClass("active");
+  $(".nav .active").removeClass("active");
+  $("#" + key).addClass("active");
 }
 
-$(function () {
-    $(".speaker").hover(function () {
-    	$(this).addClass("hover");
-    }, function () {
-    	$(this).removeClass("hover");
-    })
-    $("#directory .well").hover(function () {
-      $(this).addClass("hover");
-    }, function () {
-      $(this).removeClass("hover");
-    })
+$(function() {
+  $(".speaker").hover(function() {
+    $(this).addClass("hover");
+  }, function() {
+    $(this).removeClass("hover");
+  })
+  $("#directory .well").hover(function() {
+    $(this).addClass("hover");
+  }, function() {
+    $(this).removeClass("hover");
+  })
+  setTimeout(function() {
+    console.log('fade to black');
+    $('#logo').addClass('toblack');
+    $('.navbar').addClass('toblack');
 
+  }, 5 * 60 * 1000);
 });
